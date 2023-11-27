8b1dd7c75763b8e018434464cb60d46f5d73d16d
RAFILKLDEV <rafaelalmeidapenha@gmail.com>
2023-11-27 14:37:36 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Footer } from "./components/footer";
-import { Menu } from "./components/menu";
 import { GoogleMaps } from "./components/googleMaps";
 import { Menu2 } from "./components/menu copy";
 
